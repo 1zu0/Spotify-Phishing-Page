c06479459e0e633ee3ff4837cd4ff65e6d9e42e1
1zu0 <67436621+1zu0@users.noreply.github.com>
2020-06-25 21:30:52 +0200

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,12 @@
+<footer>
+		<div class="container pageFooter">
+			
+			<div id="contentCurve"></div>
+
+<p class="text-muted disclaimer text-center ng-binding" ng-bind-html="'loginTermsOfService' | localize:termsAndConditionsUrl:privacyPolicyUrl" style="font-size: 11px">If you are not a Spotify user and you will be registered, you have to agree to Spotify's <a href="https://www.spotify.com/legal/end-user-agreement/plain/" target="_blank">Terms & Conditions</a> and <a href="https://www.spotify.com/legal/privacy-policy/plain/" target="_blank">Privacy Policy</a>.</p>
+
+		</div>
+	</footer>
+
+</body>
+</html>
